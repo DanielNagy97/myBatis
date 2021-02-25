febca106816500adc11f45d8798ba1106b96e9f7
danielnagy97 <nagydaniel.z97@gmail.com>
2021-02-25 17:20:36 +0100

    sampleSQL corrected

--- docs/sampleSQL.sql
+++ docs/sampleSQL.sql
@@ -16,12 +16,3 @@ INSERT INTO ALBUMS VALUES ('1A 062-97176','A Night At The Opera','Queen','Rock',
 INSERT INTO ALBUMS VALUES ('LP 55 5897','Vol 4','Black Sabbath','Metal', 1975, 'Vinyl', 25);
 INSERT INTO ALBUMS VALUES ('BMC 76','Chronicle (The 20 Greatest Hits)','Creedence Clearwater Revival','Blues', 1976, 'Cassette', 5);
 INSERT INTO ALBUMS VALUES ('7243 8 32700 2 6','Pulse','Pink Floyd','Rock', 1995, 'CD', 10);
-
-
-Chronicle (The 20 Greatest Hits) 
-
-CREATE TABLE ARTISTS{
-    ID int PRIMARY KEY,
-    NAME varchar(100),
-    COUNTRY varchar(50)
-};
